6f055ebd267e92166235015c3ff09763299eca77
Monica <mcl018@ucsd.edu>
2015-04-02 12:24:33 -0700

    add node js

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+
+exports.view = function(req, res){
+	res.render('index');
+}
+
+exports.viewSpec = function(req, res){
+	res.render('Project_Spec');
+}
+
+exports.viewTeam = function(req, res){
+	res.render('Team');
+}
